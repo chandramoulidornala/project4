4a7744e81848a5e0df99b3053f91181472cb45be
chandramoulidornala <chandramouli.dornala@gmail.com>
2020-07-13 18:23:17 +0530

    intial commit

---
+++ project4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace project4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,rev,temp,sum=0;
+            Console.WriteLine("enter the string");
+            num = Convert.ToInt16(Console.ReadLine());
+                temp = num;
+            while (num > 0)
+            {
+                rev = num % 10;
+                sum = (sum * 10) + rev;
+                num = num / 10;
+            }
+            if (temp == sum)
+                Console.WriteLine("Nmber is pallindrome");
+            else
+                Console.WriteLine("It is not pallindrome");
+            }
+        }
+    }
+
